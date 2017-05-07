956d9cc13da7dac3d87281f033833b7d989431bf
sqq <shenqianqian12@gmail.com>
2017-05-07 21:46:52 +0800

    完善FlowerFragment

--- app/src/main/java/com/jet/slaveofflower/Fragment/FlowerFragment.java
+++ app/src/main/java/com/jet/slaveofflower/Fragment/FlowerFragment.java
@@ -20,6 +20,11 @@ import butterknife.ButterKnife;
 public class FlowerFragment extends BaseFragment {
     public Button know_more_caoben;
     public Button know_more_lan;
+    public Button know_more_muben;
+    public Button know_more_qiugen;
+    public Button know_more_shuisheng;
+    public Button know_more_tengben;
+    public Button know_more_sugen;
     public static FlowerFragment newInstance(){
         Bundle args=new Bundle();
         FlowerFragment fragment=new FlowerFragment();
@@ -52,6 +57,51 @@ public class FlowerFragment extends BaseFragment {
             public void onClick(View v){
                 Intent intent=new Intent(getActivity(), WebviewActivity.class);
                 intent.putExtra(WebviewActivity.EXTRA_URL,"http://www.huabaike.com/lkzw");
+                startThActivityByIntent(intent);
+            }
+        });
+        know_more_muben=(Button)view.findViewById(R.id.know_more_muben);
+        know_more_muben.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(),WebviewActivity.class);
+                intent.putExtra(WebviewActivity.EXTRA_URL,"http://www.huabaike.com/mbzw");
+                startThActivityByIntent(intent);
+            }
+        });
+        know_more_qiugen=(Button)view.findViewById(R.id.know_more_qiugen);
+        know_more_qiugen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(),WebviewActivity.class);
+                intent.putExtra(WebviewActivity.EXTRA_URL,"http://www.huabaike.com/qgzw");
+                startThActivityByIntent(intent);
+            }
+        });
+        know_more_shuisheng=(Button)view.findViewById(R.id.know_more_shuisheng);
+        know_more_shuisheng.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(),WebviewActivity.class);
+                intent.putExtra(WebviewActivity.EXTRA_URL,"http://www.huabaike.com/sszw");
+                startThActivityByIntent(intent);
+            }
+        });
+        know_more_sugen=(Button)view.findViewById(R.id.know_more_sugen);
+        know_more_sugen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(),WebviewActivity.class);
+                intent.putExtra(WebviewActivity.EXTRA_URL,"http://www.huabaike.com/sgzw");
+                startThActivityByIntent(intent);
+            }
+        });
+        know_more_tengben=(Button)view.findViewById(R.id.know_more_tengben);
+        know_more_tengben.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getActivity(),WebviewActivity.class);
+                intent.putExtra(WebviewActivity.EXTRA_URL,"http://www.huabaike.com/tbzw");
                 startThActivityByIntent(intent);
             }
         });
